488334d9b4d7e9f6e00c85f0875000d440dffa62
chilejay7 <codyburk7@yahoo.com>
2024-01-19 14:40:01 -0700

    imports from react router dom added to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Imports the built-in resources from the React libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Imports bootstrap for added styling options.
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Imports pages needed to properly route requests.
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
