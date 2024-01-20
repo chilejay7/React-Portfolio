7ff419179e1ce21be26ab69be220d5da6e250614
chilejay7 <codyburk7@yahoo.com>
2024-01-20 00:36:56 -0700

    comments added to footer

--- src/components/UI/SocialIcons.jsx
+++ src/components/UI/SocialIcons.jsx
@@ -2,8 +2,6 @@ import { SocialIcon } from 'react-social-icons';
 
 function SocialIcons({ urls }) {
 
-    // console.log(`The urls prop is: ${urls}`);
-
     return (
         <div className="social-icons">
 
@@ -12,7 +10,7 @@ function SocialIcons({ urls }) {
                 // A return inside of the map function is needed to return the JSX elements.
                 // Each list item needs a unique key.  The address is used for this purpose.
                 return (
-                        <SocialIcon key={address} url={address} />
+                    <SocialIcon key={address} url={address} />
                 )
             }
             )}
