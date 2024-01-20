c204a81e0905600e23c0ac97b6e65c10b5a0bd05
chilejay7 <codyburk7@yahoo.com>
2024-01-19 23:00:23 -0700

    social icon package added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
+import { SocialIcon } from 'react-social-icons';
+
 import './Footer.css';
+import SocialIcons from './UI/SocialIcons';
+
+
+const urls = [
+    'https://www.linkedin.com/in/cody-burkholder-4b072848/',
+    'https://github.com/chilejay7',
+]
 
 function Footer() {
     return(
         <footer className="footer">
             <h4>&#xa9; Cody Burkholder</h4>
             <p>Software Enginner 2024</p>
+
+            <SocialIcons urls={urls} />
         </footer>
     );
 };
---
+++ src/components/UI/SocialIcons.jsx
@@ -0,0 +1,10 @@
+function SocialIcons() {
+
+    return(
+        <div className="social-icons">
+            <h4>Icons</h4>
+        </div>
+    );
+};
+
+export default SocialIcons;
