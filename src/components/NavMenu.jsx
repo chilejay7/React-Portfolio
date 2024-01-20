dd81ddb8e7849d8e52c6973cc2e345d545b97e64
chilejay7 <codyburk7@yahoo.com>
2024-01-20 16:44:22 -0700

    image of resume added

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -4,7 +4,7 @@ import NavBar from "./UI/NavBar";
 function NavMenu() {
     return (
         <NavBar links={[
-            <Link key={1} to="/home">Home</Link>,
+            // <Link key={1} to="/home">Home</Link>,
             <Link key={2} to="/portfolio">Portfolio</Link>,
             <Link key={3} to="/contact">Contact</Link>,
             <Link key={4} to="/">About</Link>,
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,8 +1,16 @@
+import './Resume.css'
+
 function Resume() {
     return (
         <>
             <h2 className="title">Resume</h2>
             <hr></hr>
+
+            <section className="resume">
+
+                <img src="src\assets\resume_image.jpg" alt="image of resume"></img>
+
+            </section>
         </>
     );
 };
