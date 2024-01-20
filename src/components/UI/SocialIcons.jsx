348b6f20de61b5179a19c5f4cbd3b0faa2f5cd90
chilejay7 <codyburk7@yahoo.com>
2024-01-20 01:05:01 -0700

    conditional added to change background color of github icon

--- src/components/UI/SocialIcons.jsx
+++ src/components/UI/SocialIcons.jsx
@@ -8,12 +8,15 @@ function SocialIcons({ urls }) {
             {urls.map((address) => {
                 console.log(address);
                 // A return inside of the map function is needed to return the JSX elements.
-                // Each list item needs a unique key.  The address is used for this purpose.
+                // Each list item needs a unique key. The address is used for this purpose.
                 return (
-                    <SocialIcon key={address} url={address} />
-                )
-            }
-            )}
+                    address.includes('github') ? (
+                        <SocialIcon bgColor="blue" key={address} url={address} />
+                    ) : (
+                        <SocialIcon key={address} url={address} />
+                    )
+                );
+            })}
         </div>
     );
 };
