ea55a5d087e92d8370936d3478fdfb5061e98ec3
chilejay7 <codyburk7@yahoo.com>
2024-01-22 22:25:20 -0700

    project cards added

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,6 +1,21 @@
 import Apps from "../components/Apps";
 import './PortfolioPage.css';
 
+const projects = [
+    {
+        title: "Satellite Seeker",
+        description: "The Satellite application uses the Model View Controller (MVC) paradigm to create and maintain a site for end users to track and view information related to satellites.",
+        image: '/assets/satellite_seeker.gif',
+        url: "https://satellite-seeker-6eb766f2c5fc.herokuapp.com/",
+    },
+    {
+        title: "It's Always Sunny In...(Weather Forecast App)",
+        description: "A client who travels often requested a weather application that was capable of providing the forecast for a given location in order to better plan trips.",
+        image: "/assets/always_sunny.png",
+        url: "https://chilejay7.github.io/Its-Always-Sunny-in-Weather-App/",
+    },
+]
+
 function Portfolio() {
     return (
         <>
@@ -9,7 +24,7 @@ function Portfolio() {
 
         <section className="apps">
 
-        <Apps />
+        <Apps projects={ projects } />
 
         </section>
 
