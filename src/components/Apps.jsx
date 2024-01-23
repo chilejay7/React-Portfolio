5cb763538aaeb1e948c927683d6de91760bb06d5
chilejay7 <codyburk7@yahoo.com>
2024-01-22 23:26:34 -0700

    github icon links added to project cards

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -1,3 +1,5 @@
+import { SocialIcon } from "react-social-icons";
+
 function Apps({ projects }) {
     console.log(projects);
 
@@ -11,6 +13,7 @@ function Apps({ projects }) {
                         <p className="card-text">{p.description}</p>
                         <a href={p.url} className="btn btn-primary">Deployed App: {p.title}</a>
                     </div>
+                    <SocialIcon url={p.url} />
                 </div>
                 )
             })
