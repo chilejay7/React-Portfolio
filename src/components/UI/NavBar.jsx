33d602d248eabde541dd81c51f242a7d4bd27a7e
chilejay7 <codyburk7@yahoo.com>
2024-01-23 22:02:00 -0700

    styles moved to appropriate component files

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -1,10 +1,20 @@
 function NavBar({ links }) {
     return (
-        <nav className="nav">
-            <ul className="navList">
-                {links.map((link) => link)}
-            </ul>
-        </nav>
+        // <nav className="nav">
+        //     <ul className="navList">
+        //         {links.map((link) => link)}
+        //     </ul>
+        // </nav>
+
+        <ul class="nav nav-pills nav-justified">
+
+            {links.map((link) =>
+                <li class="nav-item">
+                    {link}
+                </li>
+            )}
+
+        </ul>
     )
 }
 
