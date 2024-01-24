f96d221c700de77f371b58687ac8a12f475af88c
chilejay7 <codyburk7@yahoo.com>
2024-01-24 00:02:15 -0700

    footer position change

--- src/components/HeaderSidebar.jsx
+++ src/components/HeaderSidebar.jsx
@@ -16,7 +16,7 @@ function HeaderSidebar() {
             <ul className="nav nav-pills flex-column mb-auto">
                 <li className="nav-item">
                     <a href="/" className="nav-link active" aria-current="page">
-                        <svg className="bi me-2" width="16" height="16"><use xlinkHref="/about"></use></svg>
+                        <svg className="bi me-2" width="16" height="16"><use xlinkHref="#bootsrap"></use></svg>
                         About
                     </a>
                 </li>
